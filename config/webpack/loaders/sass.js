08300e1c2763788e9ee09e6f1f2ba232ed88c1bf
Zachery Moneypenny <whazzmaster@gmail.com>
2017-06-02 09:35:42 -0500

    Adds css hot loading

--- config/webpack/loaders/sass.js
+++ config/webpack/loaders/sass.js
@@ -3,12 +3,19 @@ const { env } = require('../configuration.js')
 
 module.exports = {
   test: /\.(scss|sass|css)$/i,
-  use: ExtractTextPlugin.extract({
+  use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
     fallback: 'style-loader',
     use: [
-      { loader: 'css-loader', options: { minimize: env.NODE_ENV === 'production' } },
+      {
+        loader: 'css-loader',
+        options: {
+          minimize: env.NODE_ENV === 'production' ,
+          modules: true,
+          localIdentName: '[path][name]__[local]--[hash:base64:5]',
+        }
+      },
       'postcss-loader',
       'sass-loader'
     ]
-  })
+  }))
 }
