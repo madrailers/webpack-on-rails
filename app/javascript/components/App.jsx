8e29a3980c776fa1f3d7a63625411e5ccc4e5ec1
Zachery Moneypenny <whazzmaster@gmail.com>
2017-06-05 12:25:20 -0500

    Removes console log

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import styles from './App.scss';
 
-console.log(styles);
-
 const App = () => (
   <div className={styles.app}>
     <h2>Hello, Hot-Module Reloading</h2>
   </div>
-);
+)
 
 export default App;
